f2d239e31b9c08f4735ffdb88858e513c295cf4b
Rayhan <rayhan@badr.co.id>
2021-10-08 18:41:29 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Container } from "./components/Container";
+
 function App() {
 	return (
 		<div className="App">
-			<div className="text-3xl">GDG Devfest 2021 Jakarta</div>
+			<header className="shadow-md py-5">
+				<Container className="flex items-center justify-between">
+					<div>
+						<a href="#">
+							<img
+								className="w-28"
+								src={require("./assets/images/gdg-logo.png").default}
+								alt="GDG Logo"
+							/>
+						</a>
+					</div>
+					<nav>
+						<a
+							className="text-gray-500 hover:text-blue-800 transition-all py-5"
+							href="#"
+						>
+							Registration
+						</a>
+					</nav>
+				</Container>
+			</header>
 		</div>
 	);
 }
