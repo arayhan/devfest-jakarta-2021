f231b273e2040eca54ca3376ab6d1afde24d685b
Rayhan <rayhan@badr.co.id>
2021-10-08 20:58:55 +0700

    add footer section

--- src/App.js
+++ src/App.js
@@ -282,6 +282,24 @@ function App() {
 					</div>
 				</Container>
 			</section>
+
+			<footer>
+				<div className="bg-blue-700">
+					<Container className="text-center space-x-3 text-white py-10">
+						<a href="https://developers.google.com/community/gdg/organizers#community-guidelines">
+							Code of Conduct
+						</a>
+						<a href="https://developers.google.com/community-guidelines">
+							Community Guidelines
+						</a>
+					</Container>
+				</div>
+				<div className="bg-blue-800">
+					<Container className="text-center text-white py-3">
+						<div>&copy; Copyright 2021 GDG Jakarta | All Right Reserved</div>
+					</Container>
+				</div>
+			</footer>
 		</div>
 	);
 }
