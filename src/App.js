fa788aa54c9b0616733c0ad131f4a6392aae7a96
Rayhan <rayhan@badr.co.id>
2021-10-08 19:09:11 +0700

    add event description icon on banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Container } from "./components/Container";
 import ReactPlayer from "react-player";
+import { MdCalendarToday, MdLocationPin, MdSchedule } from "react-icons/md";
+import { Container } from "./components/Container";
 
 function App() {
 	return (
@@ -28,13 +29,33 @@ function App() {
 
 			<div className="py-20">
 				<Container className="flex items-center">
-					<div className="space-y-10">
+					<div className="space-y-8">
 						<div className="space-y-3">
 							<h1 className="text-3xl ">GDG Devfest 2021 Jakarta</h1>
 							<p className="text-xl text-gray-500">
 								"How to use technology to recover and boost community engagement
 								because of pandemic"
 							</p>
+						</div>
+						<div className="space-y-2">
+							<div className="flex items-center text-gray-500 space-x-3">
+								<div className="w-8 flex justify-center">
+									<MdCalendarToday size={20} />
+								</div>
+								<span className="text-base">27 November 2021</span>
+							</div>
+							<div className="flex items-center text-gray-500 space-x-3">
+								<div className="w-8 flex justify-center">
+									<MdSchedule size={24} />
+								</div>
+								<span className="text-base">2 PM to 4 PM</span>
+							</div>
+							<div className="flex items-center text-gray-500 space-x-3">
+								<div className="w-8 flex justify-center">
+									<MdLocationPin size={24} />
+								</div>
+								<span className="text-base">Live on Youtube</span>
+							</div>
 						</div>
 						<button className="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition-all">
 							Register
