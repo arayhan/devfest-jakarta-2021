0907f853b24c97e42b9540cf29e22ebf9faeab22
Rayhan <rayhan@badr.co.id>
2021-10-09 13:15:17 +0700

    update "technologies" section, set icon to link

--- src/components/sections/Topics.jsx
+++ src/components/sections/Topics.jsx
@@ -14,38 +14,58 @@ export const Topics = () => {
 					</p>
 				</div>
 				<div className="grid grid-cols-2 md:grid-cols-4 gap-8 items-center justify-center text-center">
-					<div className="flex flex-col items-center justify-center space-y-6">
+					<a
+						className="flex flex-col items-center justify-center space-y-6"
+						href="https://developer.android.com/"
+						target="_blank"
+						rel="noreferrer"
+					>
 						<img
 							className="w-28"
 							src="https://upload.wikimedia.org/wikipedia/commons/6/64/Android_logo_2019_%28stacked%29.svg"
 							alt=""
 						/>
 						<div className="text-lg text-blue-500">Android</div>
-					</div>
-					<div className="flex flex-col items-center justify-center space-y-6">
+					</a>
+					<a
+						className="flex flex-col items-center justify-center space-y-6"
+						href="https://cloud.google.com/"
+						target="_blank"
+						rel="noreferrer"
+					>
 						<img
 							className="w-28"
 							src="https://lirp.cdn-website.com/aa0ef369/dms3rep/multi/opt/google-cloud-icon-400w.png"
 							alt=""
 						/>
 						<div className="text-lg text-blue-500">Google Cloud</div>
-					</div>
-					<div className="flex flex-col items-center justify-center space-y-6">
+					</a>
+					<a
+						className="flex flex-col items-center justify-center space-y-6"
+						href="https://www.tensorflow.org/"
+						target="_blank"
+						rel="noreferrer"
+					>
 						<img
 							className="w-28"
 							src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/1200px-Tensorflow_logo.svg.png"
 							alt=""
 						/>
 						<div className="text-lg text-blue-500">Tensorflow</div>
-					</div>
-					<div className="flex flex-col items-center justify-center space-y-6">
+					</a>
+					<a
+						className="flex flex-col items-center justify-center space-y-6"
+						href="https://web.dev/"
+						target="_blank"
+						rel="noreferrer"
+					>
 						<img
 							className="w-28"
 							src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/2048px-Google_Chrome_icon_%28September_2014%29.svg.png"
 							alt=""
 						/>
 						<div className="text-lg text-blue-500">Web</div>
-					</div>
+					</a>
 				</div>
 			</Container>
 		</section>
