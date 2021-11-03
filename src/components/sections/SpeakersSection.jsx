0cb2db14aec83403397800ac3726a0e3b5bf6f22
Rayhan <rayhan@badr.co.id>
2021-11-03 19:50:25 +0700

    update SpeakerSection component

--- src/components/sections/SpeakersSection.jsx
+++ src/components/sections/SpeakersSection.jsx
@@ -5,13 +5,13 @@ import { ROUTES } from "../../routes";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { FiChevronRight } from "react-icons/fi";
 
-export const SpeakersSection = () => {
+export const SpeakersSection = ({ showAll }) => {
 	return (
 		<section className="py-24">
 			<Container className="space-y-14">
 				<div className="space-y-3">
 					<h1 className="text-4xl text-blue-700 font-semibold">
-						Featured Speakers
+						{showAll ? "Speakers" : "Featured Speakers"}
 					</h1>
 					<p className="text-lg text-gray-500">
 						Our speakers are influential folks & allies who have been associated
@@ -21,8 +21,8 @@ export const SpeakersSection = () => {
 				</div>
 
 				<div className="flex justify-center flex-wrap">
-					{SPEAKERS_DATA.map(
-						(speaker, index) =>
+					{SPEAKERS_DATA.map((speaker, index) =>
+						!showAll ? (
 							index < 4 && (
 								<SpeakerCard
 									className="sm:w-1/3 lg:w-1/4 p-3"
@@ -34,20 +34,32 @@ export const SpeakersSection = () => {
 									social_media={speaker.social_media}
 								/>
 							)
+						) : (
+							<SpeakerCard
+								className="sm:w-1/3 lg:w-1/4 p-3"
+								name={speaker.name}
+								institution={speaker.institution}
+								topic={speaker.topic}
+								avatar_url={speaker.avatar_url}
+								roles={speaker.roles}
+								social_media={speaker.social_media}
+							/>
+						)
 					)}
 				</div>
-
-				<div className="flex items-center justify-start">
-					<Link
-						className="group transition-all px-5 py-2 text-blue-700 rounded-md flex items-center space-x-5"
-						to={ROUTES.SPEAKERS}
-					>
-						<span>See All Speakers</span>
-						<span className="transition-all transform group-hover:pl-2">
-							<FiChevronRight />
-						</span>
-					</Link>
-				</div>
+				{!showAll && (
+					<div className="flex items-center justify-start">
+						<Link
+							className="group transition-all px-5 py-2 text-blue-700 rounded-md flex items-center space-x-5"
+							to={ROUTES.SPEAKERS}
+						>
+							<span>See All Speakers</span>
+							<span className="transition-all transform group-hover:pl-2">
+								<FiChevronRight />
+							</span>
+						</Link>
+					</div>
+				)}
 			</Container>
 		</section>
 	);
