55efb060d38a84617c05fabdfb15cb221a115389
Rayhan <rayhan@badr.co.id>
2021-11-03 19:34:11 +0700

    update CTA section : wording

--- src/components/sections/CTA.jsx
+++ src/components/sections/CTA.jsx
@@ -4,15 +4,16 @@ export const CTA = () => {
 	return (
 		<section className="py-24 bg-gray-100">
 			<Container className="space-y-10 text-center">
-				<div className="space-y-3">
-					<h1 className="text-3xl text-blue-700 font-semibold">
-						Get notified about the important conference updates
+				<div className="space-y-5">
+					<h1 className="text-3xl text-gray-700 font-semibold">
+						Keep in touch with{" "}
+						<span className="text-blue-700">#DevFestJakarta</span> for the
+						latest announcements
 					</h1>
-					<p className="text-lg text-gray-500">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-						maiores laborum blanditiis earum rerum inventore atque soluta
-						suscipit unde pariatur repudiandae numquam cum omnis laudantium
-						doloribus, velit non dolorum? Sapiente.
+					<p className="text-xl text-gray-500">
+						Let's get connected so that we can reach out to each other via a
+						simple notification. Stake out #DevFestJakarta for the updates and
+						news.
 					</p>
 				</div>
 				<div>
