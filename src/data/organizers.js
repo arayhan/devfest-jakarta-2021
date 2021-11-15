c0950984645076dadec4f809b7a8fdfd919dd0a9
Rayhan <rayhan@badr.co.id>
2021-11-15 18:51:46 +0700

    update organizers data

---
+++ src/data/organizers.js
@@ -0,0 +1,98 @@
+export const ORGANIZERS_DATA = [
+	{
+		name: "Muh Isfhani Ghiath",
+		role: "Publisher",
+		avatar_url:
+			"https://media-exp1.licdn.com/dms/image/C5603AQE5ZmXKWUtdFg/profile-displayphoto-shrink_200_200/0/1617364445796?e=1642636800&v=beta&t=3gI29iIezdSbjufj8xRWpf8aL6XwC3KQR996NZFB0Is",
+		social_media: {
+			linkedin: "https://www.linkedin.com/in/isfaaghyth/",
+		},
+	},
+	{
+		name: "Masqod",
+		role: "Speaker - Mobile",
+		avatar_url:
+			"https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg",
+		social_media: null,
+	},
+	{
+		name: "Toyib Nurseha",
+		role: "Speaker - Web",
+		avatar_url:
+			"https://media-exp1.licdn.com/dms/image/C5603AQHizAB3MrYC1Q/profile-displayphoto-shrink_200_200/0/1632736753957?e=1642636800&v=beta&t=K1hd0qiLfIP92WCQcHDFevNvs8H0mqrcoLrJpsnSS40",
+		social_media: {
+			linkedin: "https://www.linkedin.com/in/toyib-nurseha/",
+		},
+	},
+	{
+		name: "Alzahid Muhasabah",
+		role: "Speaker - AI/ML",
+		avatar_url:
+			"https://media-exp1.licdn.com/dms/image/C5603AQEv6SqIcJhrYQ/profile-displayphoto-shrink_200_200/0/1592725677405?e=1642636800&v=beta&t=c8d-czlx8G404VqRLkaq6neVQgiqn3H5_NlmBKvFLwM",
+		social_media: {
+			linkedin: "https://www.linkedin.com/in/alzahid-muhasabah/",
+		},
+	},
+	{
+		name: "Juan",
+		role: "Host Day 1 & 3",
+		avatar_url:
+			"https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg",
+		social_media: null,
+	},
+	{
+		name: "Qassandra Chaidir",
+		role: "Host Day 2",
+		avatar_url:
+			"https://media-exp1.licdn.com/dms/image/C5603AQEDJAVHX7FzjQ/profile-displayphoto-shrink_200_200/0/1634490878792?e=1642636800&v=beta&t=zgTdM8iRTbqlraXB3iEnjgGRNnGDgohpbEzAIQqHGbw",
+		social_media: {
+			linkedin: "https://www.linkedin.com/in/qassandrach/",
+		},
+	},
+	{
+		name: "R Surahutomo Aziz Pradana",
+		role: "Program",
+		avatar_url:
+			"https://internationalwomensday-2020.web.app/assets/images/teams/dana.jpg",
+		social_media: {
+			linkedin: "https://www.linkedin.com/in/retzd/",
+		},
+	},
+	{
+		name: "Ari Setiaji",
+		role: "Streamer - Streamyard",
+		avatar_url:
+			"https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg",
+		social_media: null,
+	},
+	{
+		name: "Rifa",
+		role: "Streamer - QnA Question Handler",
+		avatar_url:
+			"https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg",
+		social_media: null,
+	},
+	{
+		name: "Ahmed Rayhan Primadedas",
+		role: "Web Landing Page",
+		avatar_url:
+			"https://media-exp1.licdn.com/dms/image/C4D03AQF-60LAhdjbFQ/profile-displayphoto-shrink_200_200/0/1606208110875?e=1642636800&v=beta&t=31aVQuT1FKtIuoldBoiCmdf9zUki43Dnxm95duBif7A",
+		social_media: {
+			linkedin: "https://www.linkedin.com/in/arayhan",
+		},
+	},
+	{
+		name: "Bayu",
+		role: "Timekeeper",
+		avatar_url:
+			"https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg",
+		social_media: null,
+	},
+	{
+		name: "Agum",
+		role: "Streamer - QnA Question Gathering & Form",
+		avatar_url:
+			"https://therminic2018.eu/wp-content/uploads/2018/07/dummy-avatar.jpg",
+		social_media: null,
+	},
+];
