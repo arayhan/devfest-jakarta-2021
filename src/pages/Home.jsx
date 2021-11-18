a0e0cc1280ff0b354b0892f53ce8faa9379abb99
Rayhan <rayhan@badr.co.id>
2021-11-18 10:44:42 +0700

    remove fade effect : home -> speaker section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,20 +25,19 @@ export const Home = () => {
 				<Streams />
 			</Fade>
 
-			<Fade>
-				<section className="py-24">
-					<Container className="space-y-14">
-						<div className="space-y-3">
-							<h1 className="text-4xl text-blue-700 font-semibold">Speakers</h1>
-							<p className="text-lg text-gray-500">
-								Our speakers are influential folks & allies who have been
-								associated with communities within their organisations, cities,
-								country and beyond.
-							</p>
-						</div>
-						<SpeakersSection featured />
+			<section className="py-24">
+				<Container className="space-y-14">
+					<div className="space-y-3">
+						<h1 className="text-4xl text-blue-700 font-semibold">Speakers</h1>
+						<p className="text-lg text-gray-500">
+							Our speakers are influential folks & allies who have been
+							associated with communities within their organisations, cities,
+							country and beyond.
+						</p>
+					</div>
+					<SpeakersSection featured />
 
-						{/* <div className="flex items-center justify-start">
+					{/* <div className="flex items-center justify-start">
 							<Link
 								className="group transition-all px-5 py-2 text-blue-700 rounded-md flex items-center space-x-5 hover:bg-gray-50"
 								to={ROUTES.SPEAKERS}
@@ -49,9 +48,8 @@ export const Home = () => {
 								</span>
 							</Link>
 						</div> */}
-					</Container>
-				</section>
-			</Fade>
+				</Container>
+			</section>
 
 			<Fade>
 				<section className="py-24">
